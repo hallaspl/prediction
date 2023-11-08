1cfc03d1b295d2ba7ed6c36d57c7f5dff5cd7948
hallaspl <46907317+hallaspl@users.noreply.github.com>
2023-11-08 22:06:40 +0100

    Compare balance history (#9)

---
+++ src/prediction/_compare_history.py
@@ -0,0 +1,89 @@
+from __future__ import annotations
+import dataclasses as ds
+from itertools import chain
+from typing import List
+from datetime import date
+from .types import BalanceHistory, Comparition, Difference, Balance, Event
+
+
+class ComparatorException(Exception):
+    pass
+
+
+class BaseIsEmpty(ComparatorException):
+    pass
+
+
+class HistoryComparator:
+    def __init__(self, base: BalanceHistory, compared: BalanceHistory) -> None:
+        self.__base = base
+        self.__compared = compared
+        self.__aligned_base: List[Event] = None
+        self.__aligned_compared: List[Event] = None
+        self.__common_dates: List[date] = []
+        self.__first_date: date = None
+
+    def compare(self) -> Comparition:
+        try:
+            self.__first_date = max(self.__base.balances[0].date, self.__compared.balances[0].date)
+        except IndexError:
+            return Comparition(diffs=[], base_id=self.__base.id, compared_id=self.__compared.id)
+        self.__fill_common_dates()
+        self.__aligned_base = self.__align(self.__base.balances)
+        self.__aligned_compared = self.__align(self.__compared.balances)
+        differences = []
+        for base, compared in zip(self.__aligned_base, self.__aligned_compared):
+            if base.date < self.__first_date:
+                continue
+            assert base.date == compared.date, f"{base}, {compared}"
+            diff = Difference(date=base.date, value=compared.value - base.value)
+            differences.append(diff)
+        return Comparition(
+            diffs=differences, base_id=self.__base.id, compared_id=self.__compared.id
+        )
+
+    def __fill_common_dates(self) -> None:
+        dates = chain((b.date for b in self.__base.balances), (c.date for c in self.__compared.balances))
+        dates_set = set(dates)
+        self.__common_dates = list(d for d in dates_set if d >= self.__first_date)
+        self.__common_dates.sort()
+
+    def __align(self, balances: List[Balance]) -> List[Balance]:
+        aligned = self.__align_first_balance(balances)
+        return self.__align_to_common_dates(aligned)
+
+    def __align_first_balance(self, balances: List[Balance]) -> List[Balance]:
+        n_skipped = 0
+        first_balance = None
+        for bal in balances:
+            if bal.date <= self.__first_date:
+                first_balance = ds.replace(bal, date=self.__first_date)
+                n_skipped += 1
+        if first_balance:
+            return [first_balance] + balances[n_skipped:]
+        return balances
+
+    def __align_to_common_dates(self, source: List[Balance]) -> List[Balance]:
+        aligned = []
+        balance_idx = 0
+        date_idx = 0
+        prev_bal = None
+        while True:
+            try:
+                bal = source[balance_idx]
+            except IndexError:
+                bal = prev_bal
+            try:
+                current = self.__common_dates[date_idx]
+            except IndexError:
+                break
+            if bal.date == current:
+                aligned.append(bal)
+                prev_bal = bal
+                balance_idx += 1
+            if bal.date < current:
+                aligned.append(ds.replace(bal, date=current))
+            elif bal.date > current:
+                aligned.append(ds.replace(prev_bal, date=current))
+            date_idx += 1
+        return aligned
---
+++ test/test_prediction/test_compare_history.py
@@ -0,0 +1,269 @@
+import pytest
+from typing import List
+from datetime import date
+
+from prediction.types import BalanceHistory, HistoryId, Balance, Difference
+from prediction import HistoryComparator
+
+
+def test_compareEmpty_noDifferences():
+    history_id = HistoryId("nothin")
+    first = BalanceHistory(history_id, [])
+    second = BalanceHistory(history_id, [])
+    comparator = HistoryComparator(first, second)
+
+    result = comparator.compare()
+
+    assert not result.diffs
+
+
+def test_comapredIdsAreSaved():
+    first = BalanceHistory(HistoryId("first"), [])
+    second = BalanceHistory(HistoryId("second"), [])
+    comparator = HistoryComparator(first, second)
+
+    result = comparator.compare()
+
+    assert result.base_id == HistoryId("first")
+    assert result.compared_id == HistoryId("second")
+
+
+@pytest.mark.parametrize(
+    "base_balances, compared_balances, desired",
+    (
+        pytest.param(
+            [Balance("", date(2022, 2, 23), 5_000)],
+            [Balance("", date(2022, 10, 9), 1_300)],
+            [Difference(date(2022, 10, 9), -3_700)],
+            id="base starts first, one compared"
+        ),
+        pytest.param(
+            [Balance("", date(2022, 10, 9), 1_300)],
+            [Balance("", date(2022, 2, 23), 5_000)],
+            [Difference(date(2022, 10, 9), 3_700)],
+            id="base starts later, one compared"
+        ),
+        pytest.param(
+            [Balance("", date(2022, 2, 23), 5_000)],
+            [
+                Balance("", date(2022, 10, 9), 1_300),
+                Balance("", date(2022, 10, 10), 4_300)
+            ],
+            [
+                Difference(date(2022, 10, 9), -3_700),
+                Difference(date(2022, 10, 10), -700)
+            ],
+            id="base starts first, two compared"
+        ),
+        pytest.param(
+            [
+                Balance("", date(2022, 10, 9), 1_300),
+                Balance("", date(2022, 10, 10), 4_300)
+            ],
+            [Balance("", date(2022, 2, 23), 5_000)],
+            [
+                Difference(date(2022, 10, 9), 3_700),
+                Difference(date(2022, 10, 10), 700)
+            ],
+            id="compared start first, one compared"
+        ),
+        pytest.param(
+            [
+                Balance("", date(2022, 2, 23), 5_000),
+                Balance("", date(2022, 10, 11), 7_000)
+            ],
+            [
+                Balance("", date(2022, 10, 1), 1_300),
+                Balance("", date(2022, 10, 15), 4_300)
+            ],
+            [
+                Difference(date(2022, 10, 1), -3_700),
+                Difference(date(2022, 10, 11), -5_700),
+                Difference(date(2022, 10, 15), -2_700)
+            ],
+            id="base starts first, alterating"
+        ),
+        pytest.param(
+            [
+                Balance("", date(2022, 10, 1), 1_300),
+                Balance("", date(2022, 10, 15), 4_300)
+            ],
+            [
+                Balance("", date(2022, 2, 23), 5_000),
+                Balance("", date(2022, 10, 11), 7_000)
+            ],
+            [
+                Difference(date(2022, 10, 1), 3_700),
+                Difference(date(2022, 10, 11), 5_700),
+                Difference(date(2022, 10, 15), 2_700)
+            ],
+            id="compared starts first, alterating"
+        ),
+        pytest.param(
+            [
+                Balance("", date(2022, 10, 1), 10),
+                Balance("", date(2022, 10, 15), 40)
+            ],
+            [
+                Balance("", date(2022, 2, 23), 1_000),
+                Balance("", date(2022, 3, 11), 70)
+            ],
+            [
+                Difference(date(2022, 10, 1), 60),
+                Difference(date(2022, 10, 15), 30)
+            ],
+            id="compared starts first with two balances"
+        ),
+        pytest.param(
+            [
+                Balance("", date(2022, 2, 23), 5_000),
+                Balance("", date(2022, 10, 11), 7_000),
+            ],
+            [
+                Balance("", date(2022, 10, 1), 1_000),
+                Balance("", date(2022, 10, 11), 10_000),
+                Balance("", date(2022, 10, 15), 4_000),
+            ],
+            [
+                Difference(date(2022, 10, 1), -4_000),
+                Difference(date(2022, 10, 11), 3_000),
+                Difference(date(2022, 10, 15), -3_000),
+            ],
+            id="same date compared"
+        ),
+        pytest.param(
+            [
+                Balance("", date(2022, 10, 1), 1_000),
+                Balance("", date(2022, 10, 11), 10_000),
+                Balance("", date(2022, 10, 15), 4_000),
+            ],
+            [
+                Balance("", date(2022, 2, 23), 5_000),
+                Balance("", date(2022, 10, 11), 7_000),
+            ],
+            [
+                Difference(date(2022, 10, 1), 4_000),
+                Difference(date(2022, 10, 11), -3_000),
+                Difference(date(2022, 10, 15), 3_000),
+            ],
+            id="same date reversed"
+        ),
+        pytest.param(
+            [Balance("", date(2022, 10, 11), 1_300)],
+            [Balance("", date(2022, 2, 23), 5_000)],
+            [Difference(date(2022, 10, 11), 3_700)],
+            id="one compared starts first"
+        ),
+        pytest.param(
+            [Balance("", date(2022, 10, 11), 1_000)],
+            [
+                Balance("", date(2022, 2, 23), 5_000),
+                Balance("", date(2022, 3, 23), 8_000),
+            ],
+            [Difference(date(2022, 10, 11), 7_000)],
+            id="many compared starts first"
+        ),
+        pytest.param(
+            [Balance("", date(2022, 10, 11), 1_000)],
+            [
+                Balance("", date(2022, 2, 23), 5_000),
+                Balance("", date(2022, 3, 23), 6_000),
+                Balance("", date(2022, 10, 11), 8_000),
+            ],
+            [Difference(date(2022, 10, 11), 7_000)],
+            id="many past balances, date equal"
+        ),
+        pytest.param(
+            [
+                Balance("", date(2022, 10, 11), 10),
+                Balance("", date(2022, 11, 11), 20),
+                Balance("", date(2022, 12, 11), 30),
+            ],
+            [
+                Balance("", date(2022, 2, 23), 1_000),
+                Balance("", date(2022, 3, 23), 2_000),
+                Balance("", date(2022, 10, 11), 80),
+            ],
+            [
+                Difference(date(2022, 10, 11), 70),
+                Difference(date(2022, 11, 11), 60),
+                Difference(date(2022, 12, 11), 50),
+            ],
+            id="many base relates to last compared"
+        ),
+        pytest.param(
+            [
+                Balance("", date(2022, 10, 11), 100),
+                Balance("", date(2022, 11, 11), 200),
+                Balance("", date(2022, 12, 11), 300),
+            ],
+            [
+                Balance("", date(2022, 10, 1), 1_000),
+                Balance("", date(2022, 10, 12), 1_100),
+            ],
+            [
+                Difference(date(2022, 10, 11), 900),
+                Difference(date(2022, 10, 12), 1_000),
+                Difference(date(2022, 11, 11), 900),
+                Difference(date(2022, 12, 11), 800),
+            ],
+            id="many base after last compared"
+        ),
+        pytest.param(
+            [
+                Balance("", date(2022, 10, 11), 10),
+                Balance("", date(2022, 10, 12), 15),
+                Balance("", date(2022, 10, 13), 5),
+                Balance("", date(2022, 11, 5), 100),
+                Balance("", date(2022, 11, 15), 200),
+                Balance("", date(2022, 11, 25), 300),
+            ],
+            [
+                Balance("", date(2022, 10, 2), 100),
+                Balance("", date(2022, 10, 12), 50),
+                Balance("", date(2022, 10, 15), 0),
+                Balance("", date(2022, 10, 18), -10),
+                Balance("", date(2022, 11, 1), 10),
+                Balance("", date(2022, 11, 2), 30),
+                Balance("", date(2022, 11, 8), 100),
+                Balance("", date(2022, 11, 10), 50),
+                Balance("", date(2022, 12, 1), 0),
+                Balance("", date(2022, 12, 2), 20),
+                Balance("", date(2022, 12, 3), 80),
+            ],
+            [
+               Difference(date(2022, 10, 11), 90),
+               Difference(date(2022, 10, 12), 35),
+               Difference(date(2022, 10, 13), 45),
+               Difference(date(2022, 10, 15), -5),
+               Difference(date(2022, 10, 18), -15),
+               Difference(date(2022, 11, 1), 5),
+               Difference(date(2022, 11, 2), 25),
+               Difference(date(2022, 11, 5), -70),
+               Difference(date(2022, 11, 8), 0),
+               Difference(date(2022, 11, 10), -50),
+               Difference(date(2022, 11, 15), -150),
+               Difference(date(2022, 11, 25), -250),
+               Difference(date(2022, 12, 1), -300),
+               Difference(date(2022, 12, 2), -280),
+               Difference(date(2022, 12, 3), -220),
+            ],
+            id="complex scenario"
+        ),
+    )
+)
+def test_compare(
+    base_balances: List[Balance],
+    compared_balances: List[Balance],
+    desired: List[Difference]
+):
+    history_id = HistoryId("nothin")
+    base = BalanceHistory(history_id, base_balances)
+    compared = BalanceHistory(history_id, compared_balances)
+    comparator = HistoryComparator(base, compared)
+
+    result = comparator.compare()
+
+    for diff in desired:
+        assert diff in result.diffs
+    assert result.diffs == desired
